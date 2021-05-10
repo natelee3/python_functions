159fa423cad5c135132fa49dcf8ec29310acff16
Nate Lee <nleepercussion@gmail.com>
2021-05-10 17:06:45 -0400

    Delete function_large_exercises.py

--- python_functions/function_large_exercises.py
+++
@@ -1,9 +0,0 @@
-theBoard = {"7": " ", "8": " ", "9": " ",
-"4": " ", "5": " ", "6": " ",
-"1": " ", "2": " ", "3": " "}
-
-def printBoard(board):
-    print(board["7"] + "|" + board["8"])
-
-
-    ##in progress
